fa534b0c3e8b5256082f4624940e2bc2197ac853
Vytenis Butkevicius <vytenis.butkevicius@gmail.com>
2019-01-13 12:36:30 +0200

    add comments

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 module.exports = function (context, request, callback) {
   if (request.startsWith('lodash')) {
+    // covers import/require 'lodash' and import 'lodash-es'
     if (request === 'lodash' || request === 'lodash-es') {
       return callback(null, '_');
-    } else if (request[6] === '.' || request[9] === '.') {
+    // covers import/require from 'lodash.get' and other methods
+    } else if (request[6] === '.') {
       return callback(null, '_.' + request.split('.')[1]);
+    // covers import/require 'lodash/get' and import 'lodash-es/get'
     } else if (request[6] === '/' || request[9] === '/') {
       return callback(null, '_.' + request.split('/')[1]);
     }
